f11ebabea174c3ba1fe9ce4df61b0e06b5a0a397
MauroGerman <63725082+maurogerman@users.noreply.github.com>
2020-11-15 23:15:34 -0300

    Agregado agregarPacientes y agregarAreasMedicas

--- Main.java
+++ Main.java
@@ -1,50 +1,138 @@
 import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
 
 public class Main {
 	public static Scanner s;
 	public static SistemaInterno sI;
+	public Main() {
+	}
 	public static void main(String[] args) {
-		sI = new SistemaInterno();
 		s = new Scanner(System.in);
+		sI = new SistemaInterno();
+		menu();
 	}
 	
 	public static void menu() {
-		while(true){
-			System.out.println("Opcion 1) Brindar Turno");
-			System.out.println("Opcion 2) Consultar Turno");
-			System.out.println("Opcion 3) Concurrir Turno");
-			System.out.println("Opcion 4) Obtener Reporte de Estudios Realizados");
-			System.out.println("Ingrese una Opcion: ");
-			int opcion = 0;
-			opcion = s.nextInt();
-			switch(opcion) {
-			case 1:break;
-			case 2:{
-						System.out.println("Ingrese el numero de DNI del Paciente: ");
-						int dni = s.nextInt();
-						Turno t = sI.obtenerProximoTurno(dni);
-						System.out.println("Proximo turno del paciente: " + t.obtenerFechaYHora());
-						break;
-					}
-			case 3:{
-						System.out.println("Ingrese el numero de DNI del Paciente: ");
-						int dni = s.nextInt();
-						Turno t = sI.tieneTurnoHoy(dni);
-						if(t != null) {
-							System.out.println("El turno del paciente está programado para las " + t.getHoraInicio().toString());
-						}
-						else {
-							System.out.println("El paciente no tiene ningún turno programado en el día de la fecha");
-						}
-						break;
-					}
-			case 4:{
-						int cant = sI.cantidadEstudiosRealizados();
-						System.out.println("La cantidad de estudios realizados hasta el día de la fecha es: " + cant);
-						break;
-					}
+		while(true) {
+			System.out.println("Opcion 1) Login.");
+			System.out.println("Opcion 2) Registrar un Administrador.");
+			System.out.print("Ingrese una Opción: ");
+			int opc = s.nextInt();
+			switch(opc) {
+			case 1: pantallaLogin();break;
+			case 2: registrarAdmin();break;
+			default: System.out.println("Opción no Valida");
 			}
 		}
 	}
-}
+	public static void pantallaLogin() {
+		System.out.println("\nIngrese el nombre de usuario: ");
+		String user = s.next();
+		System.out.println("\nIngrese la constraseña: ");
+		String pass = s.next();
+		System.out.println(sI.loginAdmin(user, pass));
+	}
+	public static void registrarAdmin() {
+		System.out.println("\nIngrese el nombre de usuario: ");
+		String user = s.next();
+		System.out.println("\nIngrese la constraseña: ");
+		String pass = s.next();
+		System.out.println("\n¿Es un Administrador de Sistemas?: ");
+		boolean esSistemas = s.nextBoolean();
+		sI.registrarAdmin(user, pass, esSistemas);
+		//db.verUsuarios();
+		//System.out.println(db.esDeSistemas("nico"));
+	}
 
+	public static void crearPaciente(){
+		Paciente nuevoPaciente = new Paciente();
+
+
+		System.out.println("\nIngrese el nombre de paciente: ");
+		String nombre = s.next();
+		System.out.println("\nIngrese el apellido del paciente: ");
+		String apellido = s.next();
+		System.out.println("\nIngrese DNI: ");
+		String dni = s.next();
+		System.out.println("\nIngrese la domicilio: ");
+		String domicilio = s.next();
+		System.out.println("\nIngrese telefono: ");
+		String telefono = s.next();
+
+
+		nuevoPaciente.setNombre(nombre);
+		nuevoPaciente.setApellido(apellido);
+		nuevoPaciente.setDni(Integer.parseInt(dni));
+		nuevoPaciente.setDomicilio(domicilio);
+		nuevoPaciente.setTelefono(telefono);
+	}
+
+	public static void brindarUnTurno(){
+
+		System.out.println("\nIngrese el numero de consultorio: ");
+		String nroConsultorio = s.next();
+		System.out.println("\nIngrese costo: ");
+		String costo = s.next();
+		System.out.println("\nIngrese fecha: ");
+		String fecha = s.next();
+		System.out.println("\nIngrese la hora de inicio: ");
+		String hrInicio = s.next();
+		System.out.println("\nIngrese la hora de finalizacion: ");
+		String hrFin = s.next();
+		System.out.println("\nIngrese nombre paciente: ");
+		String nombrePaciente = s.next();
+
+		Paciente paciente = new Paciente();
+		paciente.setNombre(nombrePaciente);
+
+		SistemaInterno nuevoSistema = new SistemaInterno();
+		nuevoSistema.brindarTurno(
+			Integer.parseInt(nroConsultorio),
+			Float.parseFloat(costo),
+			parseFecha(fecha),
+			parseTiempo(hrInicio),
+			parseTiempo(hrFin),
+			paciente
+		);
+
+
+	}
+
+	/**
+     * Permite convertir un String en fecha (Date).
+     * @param fecha Cadena de fecha dd/MM/yyyy
+     * @return Objeto Date
+     */
+    public static LocalDate parseFecha(String fecha)
+    {
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        Date fechaDate = null;
+        try {
+            fechaDate = formato.parse(fecha);
+        } 
+        catch (ParseException ex) 
+        {
+            System.out.println(ex);
+        }
+        return fechaDate;
+    }
+
+    /**
+     * Permite convertir un String en localTime (Date).
+     * @return Objeto LocalTime
+     */
+    public static LocalTime parseTiempo(String tiempo)
+    {
+        SimpleDateFormat formato = new SimpleTimeFormat("hh:mm");
+        LocalTime tiempo = null;
+        try {
+            tiempo = formato.parse(tiempo);
+        } 
+        catch (ParseException ex) 
+        {
+            System.out.println(ex);
+        }
+        return tiempo;
+    }
+}
